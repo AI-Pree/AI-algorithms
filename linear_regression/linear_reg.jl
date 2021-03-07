5729cf5b24be50ad7e2846f2168b146863c8cac1
ai-pree <sexypistolcs@gmail.com>
2021-03-07 20:26:41 +1100

    added linear regression file

---
+++ linear_regression/linear_reg.jl
@@ -0,0 +1,23 @@
+### A Pluto.jl notebook ###
+# v0.12.21
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 1e957cea-7f1e-11eb-065e-6b9e6f9f0976
+md"# Linear Regression"
+
+# ╔═╡ 79c2e252-7f22-11eb-21d4-e1836ee79caa
+
+
+# ╔═╡ a1e6948c-7f21-11eb-2485-654e59594d7c
+
+
+# ╔═╡ 9570fb28-7f21-11eb-2c7a-ffa16a382522
+
+
+# ╔═╡ Cell order:
+# ╠═1e957cea-7f1e-11eb-065e-6b9e6f9f0976
+# ╠═79c2e252-7f22-11eb-21d4-e1836ee79caa
+# ╠═a1e6948c-7f21-11eb-2485-654e59594d7c
+# ╠═9570fb28-7f21-11eb-2c7a-ffa16a382522
